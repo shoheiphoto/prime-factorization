46f5e3bfb59d899cc7a57cad508e5908d8a827b5
shoheiphoto <sh12x29@gmail.com>
2021-01-03 18:12:16 +0900

    現代的なjavascriptへと書き改めた

--- js/main.js
+++ js/main.js
@@ -1,61 +1,47 @@
-function generateRandomNumber() {
+const generateRandomNumber = () => {
   return Math.round(Math.random() * 1000);
-}
+};
 
-function primeFactorization(num) {
-  let result = [];
-  if (num === 1) {
-    return [1];
-  }
-  let init = 2;
-  while (num !== 1) {
-    let i = init;
-    while (i < Number.MAX_SAFE_INTEGER) {
-      if (num % i == 0) {
-        result.push(i);
-        num /= i;
-        break;
-      }
-      i++;
-    }
-    init = i;
-  }
-  return result;
-}
+const write = (targeDOM, innerHTML) => {
+  targeDOM.innerHTML = innerHTML;
+};
 
-function isEven(num) {
+const isEven = num => {
   return num % 2 === 0;
-}
+};
 
-function isPrimeNumber(num) {
-  if (num === 2) {
-    return true;
-  }
-  if (num % 2 === 0) {
-    return false;
-  }
+const isPrimeNumber = num => {
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
   for (i = 2; i < num; i++) {
-    if (num % i === 0) {
-      return false;
-    }
+    if (num % i === 0) return false;
   }
   return true;
-}
+};
 
-function generatePrimeFactorizationResultMessage(primeNumbers) {
+const primeFactorization = num => {
+  const primeFactors = [];
+  if (num === 1) return [1];
+
+  for (i = 2; i <= num; i++) {
+    while (num % i === 0) {
+      primeFactors.push(i);
+      num /= i;
+    }
+  }
+  return primeFactors;
+};
+
+const generatePrimeFactorizationResultMessage = primeNumbers => {
   return primeNumbers.map(function (num, index) {
-    let stringNum = num.toString();
+    const stringNum = num.toString();
     if (index === 0) return stringNum;
-    return ' × ' + stringNum;
+    return ' x ' + stringNum;
   }).concat('となりますね。').join('');
-}
+};
 
-function write(targeDOM, innerHTML) {
-  targeDOM.innerHTML = innerHTML;
-}
-
-function main() {
-  let num = generateRandomNumber();
+const main = () => {
+  const num = generateRandomNumber();
   write(document.getElementById('currentNumber'), '数値は' + num + 'です。');
   if (isEven(num)) {
     write(document.getElementById('messageForEvenOrOdd'), '偶数です。<br>');
@@ -66,10 +52,10 @@ function main() {
     write(document.getElementById('messageForPrimeNumber'), '<h2>素数です！にゃにゃ</h2>');
     return;
   } else {
-    write(document.getElementById('messageForPrimeNumber'), '<p>素数ではありません</p>');
+    write(document.getElementById('messageForPrimeNumber'), '<p>素数ではありません。</p>');
   }
   write(document.getElementById('primeFactorizationTitle'), '<p>↓↓以下、素因数分解してみよう。↓↓</p>');
   write(document.getElementById('primeFactorizationResult'), generatePrimeFactorizationResultMessage(primeFactorization(num)));
-}
+};
 
 main();
