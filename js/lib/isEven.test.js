0ce2f5781be83ef50510ac333994e192d51d43c4
shoheiphoto <sh12x29@gmail.com>
2021-01-09 17:12:05 +0900

    テストコードを作成したver2

--- js/lib/isEven.test.js
+++ js/lib/isEven.test.js
@@ -4,3 +4,6 @@ const { isEven} = require('./isEven.js');
 test('偶数が渡されたとき、trueを返すこと', () => {
   expect(isEven(4)).toBeTruthy();
 });
+test('奇数が渡されたとき、falseを返すこと', () => {
+  expect(isEven(3)).toBeFalsy();
+});
--- js/lib/isPrimeNumber.js
+++ js/lib/isPrimeNumber.js
@@ -1,4 +1,9 @@
 export const isPrimeNumber = num => {
+  if (!num) throw new Error('引数を与えてください');
+  if (isNaN(num)) throw new Error('引数には数値を与えてください');
+  if (num < 0) return false;
+  if (!Number.isInteger(num)) return false;
+  if (num === 1) return false;
   if (num === 2) return true;
   if (num % 2 === 0) return false;
   for (let i = 2; i <num; i++) {
